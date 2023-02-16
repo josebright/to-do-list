d598e267248bb7cc142ff27276f900df22162037
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-02-16 23:47:48 +0300

    state action

---
+++ src/app/app-state/actions/todo1.action.ts
@@ -0,0 +1,14 @@
+import { Action } from '@ngrx/store';
+import { ListItem } from 'src/app/todo/todo.component';
+
+
+export enum ToDoActionType {
+  ADD_ITEM = '',
+}
+
+export class AddItemAction implements Action {
+  readonly type = ToDoActionType.ADD_ITEM;
+  constructor(public payload: ListItem) {}
+}
+
+export type ToDoAction = AddItemAction;
