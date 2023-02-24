434c64df43b3f899212bbd0fc12a64b22bd424a9
Uchenna Bright Ugwumadu <josebright29@gmail.com>
2023-02-24 23:25:22 +0300

    components

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { AppStateInterface } from 'src/app/store/app.states';
+import { LogOut } from 'src/app/store/actions';
+import { isAuthenticatedSelector } from 'src/app/store/selectors'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isAuthenticated$: Observable<boolean>
+
+  constructor(
+    private store: Store<AppStateInterface>,
+  ) {
+    this.isAuthenticated$ = this.store.pipe(select(isAuthenticatedSelector));
+  }
+
+  logOut(): void {
+    this.store.dispatch(LogOut())
+  }
+}
